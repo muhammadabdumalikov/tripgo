b6089e3ecd0cfaeaf462e41a7e732c4087e10f8e
muhammad <macbook_uz@Muhammads-Macbook.local>
2025-03-01 02:36:17 +0500

    fix

--- components/Home/Destination/Destination.tsx
+++ components/Home/Destination/Destination.tsx
@@ -49,7 +49,6 @@ const Destination = () => {
             src={src}
             alt={`Slide ${index + 1}`}
               fill
-              style={{borderRadius: '100%'}}
             className={`top-0 left-0 w-full h-full object-cover rounded-[30]`}
           />
          </div>
